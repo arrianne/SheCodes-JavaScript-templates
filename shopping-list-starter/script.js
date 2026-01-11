34db944ce72932219b0e0fcb9341ca8973291cb1
Arrianne <arrianneoshea@gmail.com>
2026-01-12 08:57:30 +1000

    added function for adding items to the list and then the function for updating it and displaying it on the page

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -12,6 +12,22 @@ for (const shoppingItem of shoppingListItems) {
   listElement.appendChild(itemElement);
 }
 
-const addItem = () => {};
+const addItem = () => {
+  let item = document.getElementById("new-item-text").value;
+  shoppingListItems = [...shoppingListItems, item];
+  updateItems();
+};
 
-const updateItems = () => {};
+const updateItems = () => {
+  // First we get the list element
+  let listElement = document.getElementById("shopping-list-items");
+  // Then we clear it of any existing items
+  listElement.innerHTML = "";
+
+  // Then we loop through the shopping list items and add them to the list
+  for (const shoppingItem of shoppingListItems) {
+    let itemElement = document.createElement("li");
+    itemElement.innerText = shoppingItem;
+    listElement.appendChild(itemElement);
+  }
+};

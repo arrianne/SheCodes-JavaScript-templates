e75c86860b36e955be27b027398737891f3f8f94
Arrianne <arrianneoshea@gmail.com>
2026-01-12 09:27:11 +1000

    creating array of items to add to the todo list, added function to create p element to store the descriptions also to create li to store the task and adding the new item to the list

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,15 +1,28 @@
-const addTask = () => {
+let todoTasks = [
+  "finish JavaScript homework",
+  "Clean the house",
+  "Make dinner",
+];
 
-};
+const todoList = document.getElementById("todo-list");
 
-const updateTodoList = () => {
+for (const task of todoTasks) {
+  // create a <p> element to store the task description
+  const newTodoTaskTextElement = document.createElement("p");
+  newTodoTaskTextElement.innerText = task;
 
-};
+  // create a <li> element to store the task item
+  const newTodoItemElement = document.createElement("li");
+  newTodoItemElement.appendChild(newTodoTaskTextElement);
 
-const createNewTodoItemElement = (task, index) => {
+  // add the new todo item to the todo list
+  todoList.appendChild(newTodoItemElement);
+}
 
-};
+const addTask = () => {};
 
-const toggleComplete = (index) => {
-    
-};
+const updateTodoList = () => {};
+
+const createNewTodoItemElement = (task, index) => {};
+
+const toggleComplete = (index) => {};
